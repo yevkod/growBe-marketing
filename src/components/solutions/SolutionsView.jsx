8b053fc2c7da7b8f8c625f638d0a628c271feada
yevgenkod <yevgen.nevidim@cowchain.io>
2024-02-27 00:18:23 +0200

    feat: changed paddings

--- src/components/solutions/SolutionsView.jsx
+++ src/components/solutions/SolutionsView.jsx
@@ -6,7 +6,7 @@ export const SolutionsView = () => {
     return (
         <div className='bg-[#FAFAFA] pt-[40px] lg:pt-[90px] pb-[90px] px-5 w-full overflow-hidden'>
             <div className='grid grid-cols-1 lg:grid-cols-2 max-w-[75rem] mx-auto'>
-                <div className='flex flex-col max-w-[515px] justify-center items-center lg:text-left mx-auto lg::mx-0'>
+                <div className='flex flex-col max-w-[515px] items-center lg:items-start lg:text-left mx-auto lg:mx-0'>
                     <div className='[font-size:_clamp(2em,4vw,45px)] text-center md:text-left font-bold leading-[55px] hover:scale-105 transition-all'>
                         <span>Perfect Solution For <br></br>
                             Your Business</span>
